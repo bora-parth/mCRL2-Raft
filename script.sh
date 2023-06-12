e4218e2826c03b523d87b07bcebdc0f58d726419
denewulf <boraparth02@gmail.com>
2023-06-12 13:42:41 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the files
+files=("clientCommand.mcf"  "electionSafety.mcf" "electionSafetyLiveliness.mcf" "leaderCompleteness.mcf" "leaderCompletenessLiveliness.mcf" "logMatching.mcf" "logMatchingLiveliness.mcf" "stateMachineLiveliness.mcf" "stateMachineSafety.mcf")
+
+# Iterate over the files and create tmux sessions
+for file in "${files[@]}"; do
+    session_name="${file%.*}" # Extract the session name without the file extension
+    
+    # Open a new tmux session and run the commands
+    tmux new-session -d -s "$session_name"
+    tmux send-keys -t "$session_name" 'export PATH=/scratch/mCRL2/202206.1/build/stage/bin/:$PATH' Enter
+    tmux send-keys -t "$session_name" 'cd /scratch/20201025' Enter
+    tmux send-keys -t "$session_name" "lps2pbes -v raft.lps -f $file raft_$session_name.pbes" Enter
+    tmux send-keys -t "$session_name" "pbessolvesymbolic --cached --chaining --groups=simple -v -m200 --lace-workers=3 --timings=$session_name_timing.txt -rjittyc raft_$session_name.pbes" Enter
+done
+
+# Attach to the first session
+tmux attach-session -t "${files[0]%.*}"
