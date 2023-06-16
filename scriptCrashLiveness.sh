c29b671837be790af8b6aef1639daa19b3c7d1e6
Parth Bora <64929331+bora-parth@users.noreply.github.com>
2023-06-16 21:44:18 +0200

    updated scripts

--- scriptCrashLiveness.sh
+++ scriptCrashLiveness.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the files
-files=("electionSafety.mcf" "leaderCompleteness.mcf" "logMatching.mcf" "logMatchingLiveliness.mcf" "stateMachineLiveliness.mcf" "stateMachineSafety.mcf")
+files=("clientCommand.mcf" "electionSafetyLiveliness.mcf" "leaderCompletenessLiveliness.mcf" "timeout.mcf")
 
 # Iterate over the files and create tmux sessions
 for file in "${files[@]}"; do
@@ -12,7 +12,7 @@ for file in "${files[@]}"; do
     tmux send-keys -t "$session_name" 'export PATH=/scratch/mCRL2/master/build/stage/bin/:$PATH' Enter
     tmux send-keys -t "$session_name" 'cd /scratch/20201025' Enter
     tmux send-keys -t "$session_name" "lps2pbes -v raft.lps -f $file raft_$session_name.pbes" Enter
-    tmux send-keys -t "$session_name" "pbessolvesymbolic --cached --chaining --groups=simple -v -m200 --threads=5 --timings=timing_$session_name.txt -rjittyc raft_$session_name.pbes" Enter
+    tmux send-keys -t "$session_name" "pbessolvesymbolic --cached --chaining --groups=simple -v -m200 --threads=5 --timings=timing_$session_name.txt -rjittyc -s2 raft_$session_name.pbes" Enter
 done
 
 # Attach to the first session
